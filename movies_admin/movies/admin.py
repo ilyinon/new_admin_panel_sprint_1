f565ae5e894730098a0f2004e85a84dc7da21427
Oleg Ilin <ilyinon@mail.ru>
2024-07-17 01:11:30 +0300

    fixed models

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,18 +1,32 @@
 from django.contrib import admin
-from .models import Genre, Filmwork, GenreFilmwork
+from .models import Genre, Filmwork, Person, GenreFilmwork, PersonFilmwork
 
-
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    pass  
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
 
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'modified')
+
+
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline,) 
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
 
     list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
+
     list_filter = ('type',)
     search_fields = ('title', 'description', 'id')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    list_filter = ('full_name',)
+    search_fields = ('full_name',)
--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -5,12 +5,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TimeStampedMixin(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(_('added_entry'), auto_now_add=True)
+    modified = models.DateTimeField(_('updated_entry'), auto_now=True)
 
     class Meta:
         # Этот параметр указывает Django, что этот класс не является представлением таблицы
         abstract = True
+
 
 class UUIDMixin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -18,83 +19,85 @@ class UUIDMixin(models.Model):
     class Meta:
         abstract = True
 
-# Create your models here.
+
 class Genre(UUIDMixin, TimeStampedMixin):
     name = models.CharField(_('name'), max_length=255)
     description = models.TextField(_('description'), blank=True)
+
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"genre"
-        # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры' 
+        verbose_name_plural = 'Жанры'
 
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class Filmwork(UUIDMixin, TimeStampedMixin):
     class FilmworkType(models.TextChoices):
-        MOVIE = "MV", ("movie")
-        TV_SHOW = "TV", ("tv_show")
+        MOVIE = "MV", (_('movie'))
+        TV_SHOW = "TV", (_('tv_show'))
 
     title = models.CharField(_('title'), max_length=255)
-    type = models.CharField(
-        max_length=2,
-        choices=FilmworkType.choices,
-        default=FilmworkType.MOVIE,
-    )
+    type = models.CharField(_('type'),
+                            max_length=2,
+                            choices=FilmworkType.choices,
+                            default=FilmworkType.MOVIE,
+                            )
     description = models.TextField(_('description'), blank=True)
     rating = models.FloatField(_('rating'), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
     genres = models.ManyToManyField(Genre, through='GenreFilmwork')
     certificate = models.CharField(_('certificate'), max_length=512, blank=True)
     file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
-    creation_date = models.DateTimeField()
-
+    creation_date = models.DateTimeField(_('published'))
 
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
-        db_table = "content\".\"filmwork"
-        # Следующие два поля отвечают за название модели в интерфейсе
+        db_table = "content\".\"film_work"
         verbose_name = 'Кинопроизведение'
-        verbose_name_plural = 'Кинопроизведения' 
+        verbose_name_plural = 'Кинопроизведения'
 
     def __str__(self):
-        return self.title 
-    
+        return self.title
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.TextField(_('full_name'), blank=False)
+
+    class Meta:
+        db_table = "content\".\"person"
+        verbose_name = 'Персона'
+        verbose_name_plural = 'Персоны'
+
+    def __str__(self):
+        return self.full_name
+
+
 class GenreFilmwork(UUIDMixin):
     film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
     genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = "content\".\"genre_film_work" 
-
-class Gender(models.TextChoices):
-    MALE = 'male', ('male')
-    FEMALE = 'female', ('female')
-
-# class Person(UUIDMixin, TimeStampedMixin):
-#     gender = models.TextField(('gender'), choices=Gender.choices, null=True) 
-
-# class PersonFilmwork(UUIDMixin):
-#     pass
-    # film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
-    # person = models.ForeignKey('Person', on_delete=models.CASCADE)
-    # role = models.TextField('role')
-    # created = models.DateTimeField(auto_now_add=True) 
-    # profession = models.TextField(_('profession'), choices=RoleType.choices, null=True)
-    # role = models.TextField(_('role'), null=True) 
+        db_table = "content\".\"genre_film_work"
 
 
+class PersonFilmwork(UUIDMixin):
 
+    class RoleType(models.TextChoices):
+        DIRECTOR = "DIRECTOR", (_('director'))
+        ACTOR = "ACTOR", (_('actor'))
+        PRODUCER = "PRODUCER", (_('producer'))
+        ANIMATOR = "ANIMATOR", (_('animator'))
+        AUDIO_ENGINEER = "AUDIO_ENGINEER", (_('audio_engineer'))
 
-#     operations = [
-#     migrations.RunSQL(
-#         sql="""
-#             ALTER TABLE "content"."genre_film_work" 
-#             ADD CONSTRAINT "genre_film_work_film_work_id_genre_id_uniq" 
-#             UNIQUE ("film_work_id", "genre_id");
-#         """,
-#         reverse_sql='ALTER TABLE "content"."genre_film_work" DROP CONSTRAINT genre_film_work_film_work_id_genre_id_uniq',
-#     )
-# ] 
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.CharField(_('role'),
+                            max_length=20,
+                            choices=RoleType.choices,
+                            default=RoleType.ACTOR,
+                            )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"person_film_work"
