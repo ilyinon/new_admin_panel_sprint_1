a9aabb600e799b0d381e0635df3ac902c7ce4e43
Oleg Ilin <ilyinon@mail.ru>
2024-07-18 01:03:10 +0300

    successfull load to pg

---
+++ sqlite_to_postgres/load_sqlite.py
@@ -0,0 +1,29 @@
+import sqlite3
+from contextlib import contextmanager
+
+
+@contextmanager
+def conn_context(db_path: str):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def load_from_sqlite(db_path: str, table_name_to_load: str, rows_to_fetch: str):
+    with conn_context(db_path) as conn:
+        curs = conn.cursor()
+        curs.execute(f"SELECT * FROM {table_name_to_load};")
+        results = []
+        while True:
+            data = curs.fetchmany(size=rows_to_fetch)
+            if not data:
+                return results
+            for item in data:
+                results.append(dict(item))
+
+
+if __name__ == '__main__':
+    print("Please run main.py to start data processing")
---
+++ sqlite_to_postgres/main.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, fields, asdict, astuple
+from uuid import UUID
+import datetime
+
+from load_sqlite import load_from_sqlite
+from load_pgsql import save_to_postgress
+
+
+SQLITE_NUMBER_OF_ROWS_TO_FETCH = 100
+DB_PATH = '/Users/oilyin/yandex/new_admin_panel_sprint_1/sqlite_to_postgres/db.sqlite'
+
+dsn = {
+    'dbname': 'mdb',
+    'user': 'app',
+    'password': '123qwe',
+    'host': 'localhost',
+    'port': 5432,
+    'options': '-c search_path=content',
+}
+
+
+@dataclass
+class User:
+    id: UUID
+    full_name: str
+    created: datetime.datetime
+    updated: datetime.datetime
+
+
+loaded_data = []
+
+
+def upload_data_from_sqlite_to_pgsql(dbname: str):
+    for el in load_from_sqlite(DB_PATH, 'person', SQLITE_NUMBER_OF_ROWS_TO_FETCH):
+        to_append = User(UUID(el['id']), el['full_name'], el['created_at'], 'NOW()')
+    loaded_data.append(to_append)
+    save_to_postgress(dsn, loaded_data)
+
+
+if __name__ == '__main__':
+
+    upload_data_from_sqlite_to_pgsql('person')
